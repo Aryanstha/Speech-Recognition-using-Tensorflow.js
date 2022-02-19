da966619ebe537b99365164bc16bc95a65c060f4
Aryan Shrestha <67673221+Aryanstha@users.noreply.github.com>
2022-02-19 19:27:03 +0545

    Create module.js

---
+++ module.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - device-detector-js@2.2.10
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/device-detector-js
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/device-detector-js@v2.2.10-tFRw3QLft1gST41VSGDz/mode=imports/optimized/device-detector-js.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/device-detector-js@v2.2.10-tFRw3QLft1gST41VSGDz/mode=imports,min/optimized/device-detector-js.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/device-detector-js@v2.2.10-tFRw3QLft1gST41VSGDz/dist=es2019,mode=imports/optimized/device-detector-js.js';
+export {default} from '/-/device-detector-js@v2.2.10-tFRw3QLft1gST41VSGDz/dist=es2019,mode=imports/optimized/device-detector-js.js';
