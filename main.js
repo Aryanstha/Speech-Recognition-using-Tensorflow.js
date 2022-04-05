81ad407677dc1ca716fbd55e13957f7b8961bc5e
Aryan Shrestha <67673221+Aryanstha@users.noreply.github.com>
2022-04-05 19:50:43 +0545

    error fixed

--- main.js
+++ main.js
@@ -1,133 +1,69 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
-/********************************************************************
- * Demo created by Aryan Shrestha 2022.
- ********************************************************************/
-
-const demosSection = document.getElementById('demos');
-
-var model = undefined;
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
-
-
-/*************************************
-/********************************************************************
-// Demo : Continuously grab image from webcam stream and classify it.
-// Note: You must access the demo on https for this to work:
-// https://tensorflow-js-image-classification.glitch.me/
-********************************************************************/
-
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
-
-// Check if webcam access is supported.
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
-}
-
-// Keep a reference of all the child elements we create
-// so we can remove them easilly on each render.
-var children = [];
-
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  const enableWebcamButton = document.getElementById('webcamButton');
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
-
-
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  if (!model) {
-    console.log('Wait! Model not loaded yet.')
-    return;
-  }
-  
-  // Hide the button.
-  event.target.classList.add('removed');  
-  
-  // getUsermedia parameters.
-  const constraints = {
-    video: true
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
-}
-
-
-function predictWebcam() {
-  // Now let's start classifying the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
-    
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        // Draw in top left of bounding box outline.
-        p.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
-            'top: ' + predictions[n].bbox[1] + 'px;' + 
-            'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
-
-        // Draw the actual bounding box.
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
+let recognizer;
+        let words;
+        const wordList = ["zero","one","two","three","four","five","six","seven","eight","nine", "yes", "no", "up", "down", "left", "right", "stop", "go"];
+        let modelLoaded = false;
         
-        // Store drawn objects in memory so we can delete them next time around.
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-    
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
-}
+        document.addEventListener('DOMContentLoaded', () => {
+            const wrapperElement = document.getElementById('sp-cmd-wrapper');
+            for (let word of wordList) {
+                wrapperElement.innerHTML += `<div class='col-3 col-md-6'><div id='word-${word}' class='badge'>${word}</div></div>`;
+            };
+            
+            document.getElementById("audio-switch").addEventListener('change', (event) => {
+                if(event.target.checked) {
+                    if(modelLoaded) {
+                        startListening();
+                    }else{
+                        loadModel();
+                    }
+                } else {
+                    stopListening();
+                }   
+            });
+        });
+        
+        async function loadModel() { 
+            // Show the loading element
+            const loadingElement = document.getElementById('demo-loading');
+            loadingElement.classList.remove('hidden');
+            
+            // When calling `create()`, you must provide the type of the audio input.
+            // - BROWSER_FFT uses the browser's native Fourier transform.
+            recognizer = speechCommands.create("BROWSER_FFT");  
+            await recognizer.ensureModelLoaded()
+            
+            words = recognizer.wordLabels();
+            modelLoaded = true;
+            
+            // Hide the loading element
+            loadingElement.classList.add('hidden');
+            startListening();
+        }
+        
+        function startListening() {
+            recognizer.listen(({scores}) => {
+            
+                // Everytime the model evaluates a result it will return the scores array
+                // Based on this data we will build a new array with each word and it's corresponding score
+                scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+                
+                // After that we sort the array by scode descending
+                scores.sort((s1, s2) => s2.score - s1.score);
+                
+                // And we highlight the word with the highest score
+                const elementId = `word-${scores[0].word}`;
+                document.getElementById(elementId).classList.add('active');
+                
+                // This is just for removing the highlight after 2.5 seconds
+                setTimeout(() => {
+                    document.getElementById(elementId).classList.remove('active');
+                }, 2500);
+            }, 
+            {
+                probabilityThreshold: 0.70
+            });
+        }
+        
+        function stopListening(){
+            recognizer.stopListening();
+        }
